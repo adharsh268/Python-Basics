3a08f0a1a1796b00bdca5ef2b572bc9f63c00283
ADHARSH <150665379+adharsh268@users.noreply.github.com>
2025-07-10 12:12:32 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,73 @@
+#-------variables----------
+#  my_name ='Adharsh'
+# print(my_name)
+
+# age = 21
+# print(age)
+
+# car1 ='bmw'
+# car2 ='jaguar'
+# car3 ='kia'
+# print(car1+' '+car2+' '+ car3)
+# print(type(car2))
+
+
+#---------Type conversion---------
+var1='1'
+
+x=int(var1)
+print(x) 
+print(type(x))
+
+y=float(x)
+print(y) 
+print(type(y))
+
+# z=complex(y)
+# print(z) 
+# print(type(z))
+
+#-------------------
+
+# var2='abc'
+
+# x=int(var2)
+# print(x) 
+# print(type(x))
+
+# var3='abc'
+# y=float(var3)
+# print(y) 
+# print(type(y))
+
+# var4='abc'
+# z=complex(var4)
+# print(z) 
+# print(type(z))
+
+#-----------------
+
+# num1=1+9j
+# x=str(num1)
+# print(x) 
+# print(type(x))
+
+# y=int(num1)
+# print(y) 
+# print(type(y))
+
+#--------------
+
+# num2=5.4
+# print(type(num2))
+# x=int(num2)
+# print(x)
+# print(type(x))
+
+# y=complex(num2)
+# print(y)
+# print(type(y))
+
+# z=str(num2)
+# print(z)
+# print(type(z))
---
+++ operators.py
@@ -0,0 +1,54 @@
+#-------arithmetic operators----
+# a=5
+# b=8
+# print(a+b)
+# print(b-a)
+# print(a*b)
+# print(b/a)
+# print(b%a)
+# print(b//a)
+
+#-------assignment operators-----
+# a=5
+# a+=3
+# print(a)
+# a-=3
+# print(a)
+# a*=3
+# print(a)
+# a/=2
+# print(a)
+
+#---------comparison operators------
+# a=4
+# b=9
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+
+#-------logical operators----------
+# x=5
+# print((x>3) and  (x<10))
+# print((x<3) or  (x<10))
+# print((x<3) or  (x>10))
+# print(not((x>3) and  (x<10)))
+
+
+#---------identity operators----------
+# x=['a','b','c']
+# y=['a','b','c']
+# z=x
+# print(x is z)
+# print(y is x)
+# print(x==y)
+
+# print(x is not z)
+# print(y is not x)
+
+#---------membership operators-------------
+x=['a','b','c']
+print('a' in x)
+print('d' in x)
+print('a' not in x)
+print('d' not in x)
