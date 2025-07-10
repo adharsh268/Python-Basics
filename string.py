eb0e706d2a7620110448722e3742d5af735407ee
ADHARSH <150665379+adharsh268@users.noreply.github.com>
2025-07-10 12:40:15 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,10 @@
+# str1='ernakulam south'
+
+# print(str1[2])
+# print(str1[-4])
+# print(str1[2:8])
+# print(str1[-8:-1]) #last word is not printed
+# print(str1[-8:0]) #no output---exception in python string
+
+# str2='ernakulam south ' #--a space is added at the end
+# print(str2[-9:-1])
