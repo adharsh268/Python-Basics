18c36e3e9b0eefe712311939f5b24db336fab9ab
ADHARSH <150665379+adharsh268@users.noreply.github.com>
2025-08-07 12:28:55 +0530

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,61 @@
+# from tkinter import *
+# root=Tk()      #initializing window
+
+# #--------widgets--------
+
+# #name of window
+# root.title("Demo Window")
+
+# #dimension of window
+# root.geometry('800x400')
+
+# #labels
+
+# lbl1=Label(root,text="Welcome", font=('Lucida Handwriting',14),fg='black',bg='white')
+
+# #label  position
+# # using pack()
+# # lbl1.pack()           #at top center
+# #-----------------------------------------------------------------------------------------------------------------------------------
+# # lbl=Label(root,text="Python Lobby")
+# # lbl.pack(side=LEFT)
+# # lbl.pack(side=RIGHT)
+# # lbl.pack(side=TOP)
+# # lbl.pack(side=BOTTOM)
+# #-----------------------------------------------------------------------------------------------------------------------------------
+
+# # using place()
+# lbl1.place(x=350,y=150)
+
+# # # entry widget - text whitespace
+# # entry=Entry(root)
+# # entry.place(x=200,y=100)
+
+# entry1=Entry(root,width=100)
+# entry1.place(x=100,y=50)
+
+
+# # buttons
+
+# def clickme():
+#     print("i am clicked!!!")
+# btn = Button(root,text='click me',command=clickme)       #function name in command
+# btn.pack()
+
+# # window background color
+# root.config(bg='lightblue')
+
+
+# root.mainloop()      #displaying window
+
+
+
+
+
+
+
+
+
+
+
+
